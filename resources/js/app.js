c133bcf74e5f096d7f319a228b87707e0a58ae00
patrik <patkonemcok@gmail.com>
2024-12-23 17:41:43 +0100

    calendar commit removal

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,3 @@ import './bootstrap';
 import './auth.js';
 import './theme.js';
 import './users_filter.js';
-import './calendar.js';
--- resources/views/calendar/calendar.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('structures.calendar')
-@section('content')
-    <div class="container">
-        <div id="calendar">
-            
-        </div>
-    </div>
-@endsection
